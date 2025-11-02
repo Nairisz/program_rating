dca47d6226fd034b9c4c82d78ccd8975ae8988da
Nairisz <norizzaty.norsupardi@gmail.com>
2025-11-02 16:36:00 +0800

    Refactor GA logic import in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import ga_ratings  # import GA logic
+from ga_ratings import run_genetic_algorithm # import GA logic
 
 st.title("Scheduling using Genetic Algorithm")
 
@@ -8,6 +8,6 @@ co_r = st.slider("Crossover Rate (CO_R)", 0.0, 0.95, 0.8)
 mut_r = st.slider("Mutation Rate (MUT_R)", 0.01, 0.05, 0.02)
 
 if st.button("Run Genetic Algorithm"):
-    schedule = ga_ratings.run_ga_rater(co_r, mut_r)
+    schedule = run_genetic_algorithm(co_r, mut_r)
     st.subheader("Generated Schedule")
     st.table(schedule)
