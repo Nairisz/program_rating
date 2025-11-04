15b178385c5a6f281229efd2616cd8d60a190b3c
Nairisz <norizzaty.norsupardi@gmail.com>
2025-11-04 12:58:10 +0800

    Update prog_rater_edited.py

--- prog_rater_edited.py
+++ prog_rater_edited.py
@@ -156,7 +156,8 @@ def genetic_algorithm(initial_schedule, generations=GEN, population_size=POP, cr
 
 ##################################################### RESULTS ###################################################################################
 
-st.title("📺 TV Program Schedule Optimizer")
+st.title("📺 TV Program Schedule Optimizer (Edited)")
+st.markdown(" *This is using edited CSV* ")
 
 # --- Sliders ---
 co_r = st.slider(
